c52525b89608a271f0138390fc126b512ea14033
Janudi Ranasinghe <Janudi.Ranasinghe@cloudsolution-sa.com>
2025-02-27 16:03:05 +0530

    Higher or Lower Game added

---
+++ Guess Higher or Lower/Higher or Lower.py
@@ -0,0 +1,31 @@
+import random
+import game_data
+import art
+
+answer = 'y'
+score = 0
+
+while answer == 'y':
+    random_compare = random.choice(game_data.data)
+    random_against = random.choice(game_data.data)
+    print(art.logo)
+    print(f"Compare A: {random_compare["name"]}, a {random_compare["description"]}, from {random_compare["country"]}")
+    print(art.vs)
+    print(f"Against B: {random_against["name"]}, a {random_against["description"]}, from {random_against["country"]}")
+
+    choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if choice == 'a':
+        if random_compare["follower_count"] < random_against["follower_count"]:
+            answer = 'n'
+            print(f"Sorry that's wrong. Final score: {score}")
+        else:
+            score += 1
+            answer = 'y'
+
+    if choice == 'b':
+        if random_compare["follower_count"] > random_against["follower_count"]:
+            print(f"Sorry that's wrong. Final score: {score}")
+            answer = 'n'
+        else:
+            score += 1
+            answer = 'y'
---
+++ Guess Higher or Lower/README.txt
@@ -0,0 +1,24 @@
+🎮 Higher or Lower Game 🔥
+------------------------------
+A fun Higher or Lower game built with Python! 🐍✨ Test your guessing skills by comparing the popularity of different items and predicting which one has a higher count. 🚀
+
+🔹 How It Works
+------------------
+🔸 Two options appear with numerical values (e.g., search volumes, followers, sales).
+🔸 Your task Guess which option has the higher value! 🔼🔽
+🔸 A correct guess lets you continue playing 🎯, while a wrong guess ends the game ❌.
+🔸 Try to achieve the highest streak possible! 🏆
+
+🌟 Features
+-------------
+✅ Text-based Python implementation 💻
+✅ Randomized comparisons for endless fun 🎲
+✅ Score tracking to challenge yourself 📊
+✅ Lightweight & simple – no extra libraries required 🎯
+
+🎲 How to Play
+----------------
+1. Run the script in Python 🐍
+2. Start guessing! 🤔
+3. Keep your streak going & beat your high score! 🏅
+
